85fd7d8bddfddcfcaa302c2fb98988d7428f06e8
Octopustank <octopustank@foxmail.com>
2026-06-23 14:50:06 +0800

    fix: rewrite MAX7219 soft SPI with direct register writes, match example init

--- Libraries/MAX7219/max7219.c
+++ Libraries/MAX7219/max7219.c
@@ -1,28 +1,23 @@
 #include "max7219.h"
 
-static void delay_sw(void)
-{
-    for (volatile int i = 0; i < 4; i++) __NOP();
-}
-
 static void max7219_send16(uint8_t addr, uint8_t data)
 {
     uint16_t word = ((uint16_t)addr << 8) | data;
 
-    HAL_GPIO_WritePin(MAX7219_CS_PORT, MAX7219_CS_PIN, GPIO_PIN_RESET);
+    MAX7219_CS_PORT->BRR = MAX7219_CS_PIN;
 
     for (int i = 15; i >= 0; i--) {
-        HAL_GPIO_WritePin(MAX7219_CLK_PORT, MAX7219_CLK_PIN, GPIO_PIN_RESET);
+        MAX7219_CLK_PORT->BRR = MAX7219_CLK_PIN;
         if (word & (1 << i))
-            HAL_GPIO_WritePin(MAX7219_DIN_PORT, MAX7219_DIN_PIN, GPIO_PIN_SET);
+            MAX7219_DIN_PORT->BSRR = MAX7219_DIN_PIN;
         else
-            HAL_GPIO_WritePin(MAX7219_DIN_PORT, MAX7219_DIN_PIN, GPIO_PIN_RESET);
-        delay_sw();
-        HAL_GPIO_WritePin(MAX7219_CLK_PORT, MAX7219_CLK_PIN, GPIO_PIN_SET);
-        delay_sw();
+            MAX7219_DIN_PORT->BRR  = MAX7219_DIN_PIN;
+        __NOP(); __NOP();
+        MAX7219_CLK_PORT->BSRR = MAX7219_CLK_PIN;
+        __NOP(); __NOP();
     }
 
-    HAL_GPIO_WritePin(MAX7219_CS_PORT, MAX7219_CS_PIN, GPIO_PIN_SET);
+    MAX7219_CS_PORT->BSRR = MAX7219_CS_PIN;
 }
 
 void max7219_write(uint8_t addr, uint8_t data)
@@ -32,33 +27,33 @@ void max7219_write(uint8_t addr, uint8_t data)
 
 void max7219_init(void)
 {
-    GPIO_InitTypeDef s = {0};
-
     __HAL_RCC_GPIOA_CLK_ENABLE();
 
+    GPIOA->BSRR = MAX7219_CS_PIN | MAX7219_CLK_PIN;
+
+    GPIO_InitTypeDef s = {0};
     s.Mode  = GPIO_MODE_OUTPUT_PP;
     s.Pull  = GPIO_NOPULL;
     s.Speed = GPIO_SPEED_FREQ_HIGH;
 
+    s.Pin = MAX7219_CS_PIN;
+    HAL_GPIO_Init(MAX7219_CS_PORT, &s);
     s.Pin = MAX7219_DIN_PIN;
     HAL_GPIO_Init(MAX7219_DIN_PORT, &s);
     s.Pin = MAX7219_CLK_PIN;
     HAL_GPIO_Init(MAX7219_CLK_PORT, &s);
-    s.Pin = MAX7219_CS_PIN;
-    HAL_GPIO_Init(MAX7219_CS_PORT, &s);
 
-    HAL_GPIO_WritePin(MAX7219_CS_PORT, MAX7219_CS_PIN, GPIO_PIN_SET);
     HAL_Delay(100);
 
-    max7219_send16(MAX7219_REG_DISPTEST,  0x00);
-    max7219_send16(MAX7219_REG_SCANLIMIT, 0x07);
-    max7219_send16(MAX7219_REG_DECODE,    0x00);
-    max7219_send16(MAX7219_REG_INTENSITY, 0x01);
-    max7219_send16(MAX7219_REG_SHUTDOWN,  0x01);
+    max7219_send16(0x09, 0x00);
+    max7219_send16(0x0A, 0x08);
+    max7219_send16(0x0B, 0x07);
+    max7219_send16(0x0C, 0x01);
+    max7219_send16(0x0F, 0x00);
 }
 
 void max7219_display(const uint8_t pattern[8])
 {
     for (int i = 0; i < 8; i++)
-        max7219_send16(MAX7219_REG_DIGIT1 + i, pattern[7 - i]);
+        max7219_send16(0x01 + i, pattern[i]);
 }
